bd4e1260cf676a8bf455bb1d4fced4c04d6e19a8
ththrh <44047525+ththrh@users.noreply.github.com>
2023-11-02 15:48:43 +0900

    Field, Hand

--- Assets/_KDH/Script/FieldManager.cs
+++ Assets/_KDH/Script/FieldManager.cs
@@ -1,8 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FieldManager : MonoBehaviour
 {
     public List<GameObject> fields;
+    public static FieldManager Instance;
+    public GameObject FieldPrefab;
+
+    private Vector2 instantiatePosition;
+
+    const int FULL_FIELD_COUNT = 10;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            instantiatePosition = new Vector3(mousePos.x, 0, 0);
+            RaycastHit2D rayhit = Physics2D.Raycast(mousePos, Vector2.zero);
+            if (rayhit.collider != null)
+            {
+                if (rayhit.collider.GetComponent<Field>() != null)
+                {
+                    PlaceCard(rayhit.collider.GetComponent<Field>());
+                }
+            }
+        }
+    }
+
+    void PlaceCard(Field field)
+    {
+        if (HandManager.Instance.selectedHand == null) return;
+        if (field.isEmpty)
+        {
+            field.SetCard(HandManager.Instance.selectedHand.card);
+        }
+        else
+        {
+            GameObject newField = Instantiate(FieldPrefab, instantiatePosition, Quaternion.identity);
+            newField.GetComponent<Field>().SetCard(HandManager.Instance.selectedHand.card);
+            newField.GetComponent<Field>().prevField = field.prevField;
+            newField.GetComponent<Field>().nextField = field;
+            field.prevField.nextField = newField.GetComponent<Field>();
+            field.prevField = newField.GetComponent<Field>();
+            while (field.nextField != null)
+            {
+                field.transform.position = new Vector3(field.transform.position.x + 20, field.transform.position.y);
+                field = field.nextField;
+            }
+        }
+        HandManager.Instance.RemoveHand();
+    }
+
+    bool IsFieldFull()
+    {
+        if(fields.Count == FULL_FIELD_COUNT)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 }
---
+++ Assets/_PJH/Scripts/HandCard.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CCGCard;
+using System;
+using TMPro;
+
+public class HandCard : MonoBehaviour
+{
+    [Header("Ä«µå")]
+    public Card card;
+
+    [Header("")]
+    public bool isEmpty = true;
+    public TMP_Text frontDamageText;
+    public TMP_Text BackDamageText;
+
+    private SpriteRenderer rend;
+    public bool isSelected;
+
+    private void Awake()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        DrawCardData();
+    }
+    private void Update()
+    {
+
+    }
+
+    public void SetCard(Card card)
+    {
+        if (isEmpty)
+        {
+            this.card = card;
+            isEmpty = false;
+            DrawCardData();
+        }
+    }
+
+    public void RemoveCard()
+    {
+        card = null;
+        isEmpty = true;
+        frontDamageText.text = "";
+        BackDamageText.text = "";
+        rend.sprite = null;
+    }
+
+    void DrawCardData()
+    {
+        frontDamageText.text = card.frontDamage.ToString();
+        BackDamageText.text = card.backDamage.ToString();
+
+        if(rend != null)
+        {
+            rend.sprite = card.cardSprite;
+        }
+    }
+}
