aec0fffedb92dc5b8f3e91a32e62cc759e55e973
MyeunggiKim <139723168+MyeunggiKim@users.noreply.github.com>
2023-11-14 16:05:00 +0900

    feat: 게임 페이즈 처리용 스크립트 작성 완

--- Assets/_KMK/Scripts/PaseSpriteRender.cs
+++ Assets/_KMK/Scripts/PaseSpriteRender.cs
@@ -4,10 +4,63 @@ using UnityEngine;
 
 public class PaseSpriteRender : MonoBehaviour
 {
-    public GameManager GameManager;
+    public List<Sprite> offSprite;
+    public List<GameObject> onSprite;
+    public GameManager manager;
+
+    public GamePhase gamePhase;
+
+    void Start()
+    {
+
+    }
 
     void Update()
     {
-        GameManager.Instance.gamePhase;
+        GamePhaseRender();
+    }
+
+    public void GamePhaseRender()
+    {
+        if(gamePhase != manager.gamePhase)
+        {
+            gamePhase = manager.gamePhase;
+
+            for(int i = 0; i < onSprite.Count; i++)
+            {
+                onSprite[i].SetActive(false);
+            }
+            if(gamePhase == GamePhase.DrawPhase)
+            {
+                onSprite[1].SetActive(true);
+            }
+            else if(gamePhase == GamePhase.ActionPhase)
+            {
+                onSprite[2].SetActive(true);
+            }
+            else if(gamePhase == GamePhase.BattlePhase)
+            {
+                onSprite[3].SetActive(true);
+            }
+            else if(gamePhase == GamePhase.ExecutionPhase)
+            {
+                onSprite[4].SetActive(true);
+            }
+            else if(gamePhase == GamePhase.EndPhase)
+            {
+                onSprite[5].SetActive(true);
+            }
+            else
+            {
+                Debug.Log("처리 가능한 페이즈가 아닙니다.");
+            }
+
+            PhaseChange();
+        }
+    }
+
+    public void PhaseChange()
+    {
+        gamePhase = manager.gamePhase;
     }
 }
