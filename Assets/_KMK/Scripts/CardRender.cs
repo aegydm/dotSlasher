cbd9bd36243eb305855975c5246f5bd44cce4c62
MyeunggiKim <139723168+MyeunggiKim@users.noreply.github.com>
2023-11-14 14:38:43 +0900

    1

---
+++ Assets/_KMK/Scripts/CardRender.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardRender : MonoBehaviour
+{
+    public static CardRender instance;
+
+    [SerializeField] public List<Sprite> cardFrame;
+    [SerializeField] public List<Sprite> characterSprite;
+    [SerializeField] public List<Animator> animators;
+
+    //public void 
+}
---
+++ Assets/_KMK/Scripts/PaseSpriteRender.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaseSpriteRender : MonoBehaviour
+{
+    public GameManager GameManager;
+
+    void Update()
+    {
+        GameManager.Instance.gamePhase;
+    }
+}
