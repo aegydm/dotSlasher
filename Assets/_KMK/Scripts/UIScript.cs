1c4ee441b51b4fa32b103ac5b3d474b995db1cbd
MyeunggiKim <139723168+MyeunggiKim@users.noreply.github.com>
2023-11-15 16:32:40 +0900

    feat : 스프라이트 소스 일부 변경

--- Assets/_KMK/Scripts/UIScript.cs
+++ Assets/_KMK/Scripts/UIScript.cs
@@ -5,24 +5,24 @@ using UnityEngine.UI;
 
 public class UIScript : MonoBehaviour
 {
-    [Header("¸Ş´º Ã¢")]
+    [Header("ë©”ë‰´ ì°½")]
     public GameObject soundUIBackGround;
     public GameObject optionUIBackGround;
     public GameObject blank;
     public bool windowOn = false;
     bool soundWindowOn = false;
-    [Header("Ã¢ ´İ±â")]
+    [Header("ì°½ ë‹«ê¸°")]
     public Button soundMenuCloseButton;
     public Button optionMenuCloseButton;
-    [Header("À½·® ½½¶óÀÌ´õ")]
+    [Header("ìŒëŸ‰ ìŠ¬ë¼ì´ë”")]
     public Slider mVol;
     public Slider bgmVol;
     public Slider effVol;
-    [Header("À½¼Ò°Å ¹öÆ°")]
+    [Header("ìŒì†Œê±° ë²„íŠ¼")]
     public Toggle mToggle;
     public Toggle bgmToggle;
     public Toggle effToggle;
-    [Header("¿Àµğ¿À ¼Ò½º")]
+    [Header("ì˜¤ë””ì˜¤ ì†ŒìŠ¤")]
     public AudioSource bgmPlayer;
     public AudioSource effPlayer;
 
@@ -85,7 +85,7 @@ public class UIScript : MonoBehaviour
     {
         if (mToggle.isOn)
         {
-            if (bgmToggle.isOn)
+            if (effToggle.isOn)
             {
                 effPlayer.volume = effVol.value * mVol.value;
             }
@@ -97,7 +97,7 @@ public class UIScript : MonoBehaviour
         else
         {
             mVol.value = 0;
-            bgmPlayer.volume = 0;
+            effPlayer.volume = 0;
         }
     }
 
